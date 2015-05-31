a1f50267816f09f08fdb313a448468af6dc0274c
Tyler <tslimkemann42@gmail.com>
2015-05-30 18:36:05 -0700

    finished sanitation

--- saftenString.rb
+++ saftenString.rb
@@ -1,8 +1,10 @@
-dangerousCharacters = %w(\ < > =)
-
-def saften(string) #safety, saften, hahah i'm funny
-	dangerousCharacters.each do |char|
-		string.gsub!(char, ?\ + char) #escape the dangerous character
-	end
+def jsonSanitizeString(string)
+	string.gsub!(?\\, "\\\\") #replace 1 backslash with 2
+	string.gsub!(?\n, "\\n")
+	string.gsub!(?\b, "\\b")
+	string.gsub!(?\f, "\\f")
+	string.gsub!(?\r, "\\r")
+	string.gsub!(?\t, "\\t")
+	string.gsub!("\"", ?") #replace a " with a \" (who the hell designed the ? notation :(, it autoescapes " )
 	return string
 end
