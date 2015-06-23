b06307ff5b4d4df8da8c60b0cffc26c787ae6809
unknown <tslimkemann42@gmail.com>
2015-06-23 01:46:45 -0700

    images wooooooo

---
+++ public/lengthConstants.js
@@ -0,0 +1,11 @@
+var maxCommentLength = 1000;
+var minCommentLength = 2;
+
+var maxFileSize = 4*1000000 //4 megabytes
+
+var maxTitleLength = 100;
+var minTitleLength = 2;
+
+var maxBodyLength = 5000;
+var minBodyLength = 2;
+
