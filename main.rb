ea148bf8e93efb9f31444c709bc748f350ea91c0
Tyler Limkemann <tslimkemann42@gmail.com>
2020-04-20 14:59:42 -0700

    fix

--- main.rb
+++ main.rb
@@ -34,7 +34,7 @@ get "/" do
 	erb :index
 end
 
-get /\A\/(\d{1,3})\z/ do #get any get request that"s "/<1 to 3 digit number>", like a url
+get /\/(\d{1,3})/ do #get any get request that"s "/<1 to 3 digit number>", like a url
 	erb :postview
 end
 
